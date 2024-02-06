d79efdf2476a1e54041da2a5ba3fba9b918237b0
Aryan Kumar <aryankumar877@gmail.com>
2024-02-06 17:08:37 +0530

    text color fix

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -8,7 +8,7 @@ import Image from 'next/image';
 function CoursesPage() {
   return (
     <div className="min-h-screen bg-black py-12 pt-36">
-      <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8">
+      <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">
         All Courses ({coursesData.courses.length})
       </h1>
       <div className="flex flex-wrap justify-center">
